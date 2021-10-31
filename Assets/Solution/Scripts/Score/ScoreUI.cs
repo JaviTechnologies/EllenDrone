49fe6e18d4759b6ac16d8e5f8d92bddbe01474e0
Xavi Tech <creator.of.greatness@tutanota.com>
2021-10-31 18:00:24 -0300

    Renaming folder

---
+++ Assets/Solution/Scripts/Score/ScoreUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EllenDrone
+{
+    public class ScoreUI : MonoBehaviour
+    {
+        private Text scoreText;
+
+        public GameObject scoreUIPrefab;
+
+        IEnumerator Start()
+        {
+            yield return null;
+
+            // instantiate and allocate score hud in the canvas
+            GameObject scoreUIGO = Instantiate(scoreUIPrefab, this.transform);
+            RectTransform scoreUIRect = scoreUIGO.transform as RectTransform;
+            scoreUIRect.anchorMin = Vector2.one;
+            scoreUIRect.anchorMax = Vector2.one;
+            scoreUIRect.pivot = Vector2.one;
+            scoreUIRect.anchoredPosition = Vector2.zero;
+
+            // keep local ref to the text
+            scoreText = scoreUIGO.GetComponent<Text>();
+
+            // init score text
+            scoreText.text = "Score: 0";
+        }
+
+        public void UpdateScoreUI(Scorer scorer)
+        {
+            scoreText.text = string.Format("Score: {0}", scorer.CurrentScore);
+        }
+    }
+}
+
---
+++ Assets/Solution/Scripts/Score/Scorer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace EllenDrone
+{
+    /// <summary>
+    /// Keeps tracks of the score.
+    /// It is a Singleton in order to be referenced globally.
+    /// </summary>
+    public class Scorer : MonoBehaviour
+    {
+        protected static Scorer s_Instance;
+
+        // event to notify changes on the score
+        public UnityEvent OnScore;
+
+        public static Scorer Instance { get { return s_Instance; } }
+        public int CurrentScore { get; private set; }
+
+        private void Awake()
+        {
+            if (s_Instance == null)
+                s_Instance = this;
+            else if(s_Instance != this)
+                throw new UnityException("There cannot be more than one Scorer script.  The instances are " + s_Instance.name + " and " + name + ".");
+        }
+
+        public void AddScore(int score)
+        {
+            CurrentScore += score;
+
+            if (OnScore != null)
+                OnScore.Invoke();
+        }
+    }
+}
